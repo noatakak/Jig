d195851217921a55595dd344c93a759e44422bfd
noatakak <noatak.post@gmail.com>
2023-07-22 08:11:41 -0700

    fixed pause and unpause functionality

--- scenes/game.gd
+++ scenes/game.gd
@@ -10,23 +10,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_just_pressed("pause") and !get_node("MainMenu").visible:
-		get_node("PauseWindow").visible = true
-		get_tree().paused = true
+	pass
 
 
-func _on_button_pressed():
+# Ends game and shows home menu
+func _on_menu_button_pressed():
+	midi_game.mplayer.stop()
+	midi_game.ui.visible = false
+	midi_game.visible = false
+	get_node("PauseWindow").visible = false
+	get_node("MainMenu").visible = true
+	get_tree().paused = false
+	midi_game.set_process(false)
+
+
+# Starts game and hides menu
+func _on_play_button_pressed():
 	get_node("MainMenu").visible = false
 	midi_game.visible = true
 	midi_game.set_process(true)
-	# start game
 
-
-func _on_pause_button_pressed():
-	get_node("PauseWindow").visible = false
-	get_node("MainMenu").visible = true
-	midi_game.visible = false
-	midi_game.set_process(false)
-	get_tree().paused = false
-	# stop game
-	
--- scenes/test_scene/test_game.gd
+++ scenes/test_scene/test_game.gd
@@ -1,18 +1,46 @@
 extends Node2D
 
 var mplayer
+var ui
+var buffer
+
+var key_a_flag
+var key_s_flag
+var key_d_flag
+var key_f_flag
+
+var key_a_timer
+var key_s_timer
+var key_d_timer
+var key_f_timer
+
+var note_a_flag
+var note_s_flag
+var note_d_flag
+var note_f_flag
+
+var note_a_timer
+var note_s_timer
+var note_d_timer
+var note_f_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mplayer = get_node("MidiPlayer")
+	ui = get_node("keys")
+	ui.visible = false
+	
+	buffer = 0.1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_pressed("test"):
+		ui.visible = true
 		mplayer.play()
 
 
+# gets a signal on lyric appearance
 func _on_midi_player_appeared_lyric(lyric):
 	print("lyric: " + str(lyric))
 	if Input.is_action_pressed(str(lyric)):
